2ab74f055c0ddc17c61c17310194ce27d93caf99
SQA <sqa@SQAs-MacBook-Pro.local>
2024-11-27 14:36:06 +0700

    initial commit

---
+++ mysite/api/views.py
@@ -0,0 +1,36 @@
+from django.shortcuts import render
+from rest_framework import generics, status
+from .models import BlogPost
+from .serializers import BlogPostSerializer
+from rest_framework.views import APIView
+from rest_framework.response import Response
+
+
+# Create your views here.
+class BlogPostListCreate(generics.ListCreateAPIView):
+    queryset = BlogPost.objects.all()
+    serializer_class = BlogPostSerializer
+
+    def delete(self, request, *args, **kwargs):
+        BlogPost.objects.all().delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+# to access an individual post
+class BlogPostRetrieveUpdateDestrory(generics.RetrieveUpdateDestroyAPIView):
+    queryset = BlogPost.objects.all()
+    serializer_class = BlogPostSerializer
+    lookup_field = "pk"
+
+
+class BlogPostList(APIView):
+    def get(self, request, format=None):
+        title = request.query_params.get("title", "")
+
+        if title:
+            blog_posts = BlogPost.objects.filter(title_icontains=title)
+        else:
+            blog_posts = BlogPost.objects.all()
+
+        serializer = BlogPostSerializer(blog_posts, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
